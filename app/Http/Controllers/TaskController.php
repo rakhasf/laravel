eef6ee7baeea87554a40c6e5d422e58a465909bf
rakhasf <rakhasaptafabian@gmail.com>
2023-01-27 10:05:54 +0700

    YEA

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class TaskController extends Controller
+{
+    private $taskList = [
+        'first' => 'Eat',
+        'second' => 'Work',
+        'third' => 'play'
+    ]; 
+
+    public function index(Request $request) {
+        // if ($request -> search) {
+        //     return $this -> taskList[$request -> search];
+        // }
+        // return $this -> taskList;
+        if ($request -> search) {
+            $task = DB::table('tasks')
+            ->where('task', 'LIKE', "%$request->search%")
+            ->get();
+
+            return $task;
+        }
+        $task = DB::table('tasks')->get();
+        return $task;
+        
+    }
+
+    public function show($id) {
+        $task = DB::table('tasks')
+        ->where('id', $id)
+        ->get();
+        return $task;
+    }
+
+    public function store(Request $request){
+        DB::table('tasks')
+        ->insert([
+            'task' => $request->task,
+            'user' => $request->user
+        ]);
+        return 'Success';
+    }
+    
+    public function update(Request $request, $id){
+        DB::table('tasks')
+        ->where('id', $id)
+        ->update([
+            'task' => $request->task,
+            'user' => $request->user
+        ]);
+        return 'Success';
+    }
+
+    public function delete($id) {
+        DB::table('tasks')
+        ->where('id', $id)
+        ->delete();
+        return 'Success';
+    }
+
+}
